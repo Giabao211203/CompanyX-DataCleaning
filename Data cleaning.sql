187d8a2605dae9b773dbb324e67d3f949ec05174
Giabao211203 <128055293+Giabao211203@users.noreply.github.com>
2024-12-19 21:43:51 +0700

    Add files via upload

---
+++ Data cleaning.sql
@@ -0,0 +1,15 @@
+UPDATE Sales.SalesOrderDetail
+SET OrderQty = 0
+WHERE OrderQty IS NULL;
+UPDATE Sales.SalesOrderDetail
+SET UnitPrice = 419.4589
+WHERE ProductID = 760 AND UnitPrice IS NULL;
+UPDATE Sales.SalesOrderDetail
+SET UnitPrice = 419.4589
+WHERE ProductID = 770 AND UnitPrice IS NULL;
+UPDATE Sales.SalesOrderDetail
+SET UnitPrice = 28.8404
+WHERE ProductID = 715 AND UnitPrice IS NULL;
+SELECT *
+FROM Sales.SalesOrderDetail
+WHERE ProductID = 715 AND UnitPrice = 0;
